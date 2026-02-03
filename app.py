e76d8401a5774fbf87abc61652bbcea85ac835c6
James Wol <james.wol@petalmail.com>
2026-02-03 03:15:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -280,10 +280,4 @@ for msg in site_analysis(plot_width, plot_depth, road_side):
     else:
         st.success(msg)
 
-if area > build_area:
-    st.error("❌ Design exceeds buildable plot area")
-else:
-    st.success("✔ Design fits within buildable plot area")
-
-
 
